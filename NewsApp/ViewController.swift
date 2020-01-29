67e574c063d2d375d3990dfb154fb98680fab501
rustemsu <rustem.supayev@gmail.com>
2020-01-29 16:48:46 +0600

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Rustem Supayev on 29/01/2020.
+//  Copyright © 2020 Rustem Supayev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
